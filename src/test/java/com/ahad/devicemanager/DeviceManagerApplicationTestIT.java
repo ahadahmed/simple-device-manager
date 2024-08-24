9416969932604bce7d18239d305eff833a80e743
ahadahmed <ahadahmed.career@gmail.com>
2024-08-24 17:20:57 +0200

    Refactor, renamed test class.

--- src/test/java/com/ahad/devicemanager/DeviceManagerApplicationTestIT.java
+++ src/test/java/com/ahad/devicemanager/DeviceManagerApplicationTestIT.java
@@ -27,7 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class DeviceManagerApplicationTests {
+class DeviceManagerApplicationTestIT {
 
     @Autowired
     private ObjectMapper mapper;
